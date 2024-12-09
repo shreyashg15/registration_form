ff22c45de35094dcb4fb05ab797e64826900defa
Shreyas H G <shreyashggirish1234@gmail.com>
2024-12-09 21:11:06 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -23,13 +23,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 } else {
     echo "Invalid request.";
 }
-<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Your existing PHP code
 }
-?>
 
 ?>
